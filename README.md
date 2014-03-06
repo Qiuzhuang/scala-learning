b7786aaad74cb56de995288d38a4262d819488b3
Qiuzhuang Lian <Qiuzhuang.Lian@gmail.com>
2014-03-06 17:25:54 +0800

    cracking code interview ch1 about list.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ scala-learning
 ==============
 
 To learn scala
+
+This is to learn to program Scala via solving cracking interview problems:
+
+http://hawstein.com/posts/1.1.html
--- src/com/qiuzhuang/cracking/ch1/ListProblem.scala
+++ src/com/qiuzhuang/cracking/ch1/ListProblem.scala
@@ -17,6 +17,22 @@ object ListProblem {
     if (s == null || s.size == 0) true else loop(s.head, s.tail)
   }
 
+  /**
+   * Design an algorithm and write code to remove the duplicate characters in a string without using
+   * any additional buffer. NOTE: One or two additional variables are fine.
+   * An extra copy of the array is not.
+   * FOLLOW UP: Write the test cases for this method.
+   */
+  def truncate(s: String): String = {
+    val sb = new StringBuilder
+    for (ch <- s) {
+      if (!sb.contains(ch)) {
+         sb.append(ch)
+      }
+    }
+    sb.toString
+  }
+
   def main(args: Array[String]) {
     val s0 = ""
     val s1 = "my name is kand lian"
